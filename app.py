1444ce8983a994628e1f2f652aefd491c809b519
kyra <94730682+ky-ra1@users.noreply.github.com>
2022-01-23 00:59:26 +1100

    added session, updated route & port

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session 
 import os
 
 from controllers.user_controller import user_controller
 from controllers.session_controller import session_controller
 from controllers.message_controller import message_controller
+from models.message import get_all_messages
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "password")
 
@@ -11,14 +12,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 @app.route('/')
 def index():
-  return render_template('home.html')
+  messages = get_all_messages(session.get('user_id'))
+  return render_template('home.html', messages=messages)
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
-# Don't forget to register controllers
 app.register_blueprint(user_controller)
 app.register_blueprint(session_controller)
 app.register_blueprint(message_controller)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5001, debug=True)
