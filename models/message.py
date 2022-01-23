090866e57469cf2512e96afc8d4dac8c53908627
kyra <94730682+ky-ra1@users.noreply.github.com>
2022-01-23 23:56:12 +1100

    updated about section

--- models/message.py
+++ models/message.py
@@ -11,21 +11,31 @@ def insert_message(message, user_id):
 
 # SELECT message from db (only if logged in)
 def get_message(id):
-    results = database.sql_select('SELECT id, message, user_id FROM message WHERE id = %s',[id])
+    results = database.sql_select('SELECT id, message, user_id FROM messages WHERE id = %s',[id])
     result = results[0]
     # result['loggedin'] = result[user_id]
     return result
 
+# select all messages 
+def get_all_messages(messages_user_id_fk):
+    results = database.sql_select('SELECT id, message, user_id FROM messages WHERE user_id = %s',[messages_user_id_fk])
+    # result['loggedin'] = result[user_id]
+    return results
+
 
 # UPDATE message in db
-def update_message(id, message, user_id):
-    database.sql_write("UPDATE messages set message = %s, user_id = %s WHERE id = %s", [
-        id,
+def update_message(message, id):
+    database.sql_write("UPDATE messages set message = %s WHERE id = %s", [
         message,
-        user_id
-    ])
+        id,
+        ])
+    
+   
 
-
-# DELETE message from db
+# DELETE one message from db
 def delete_message(id):
     database.sql_write("DELETE FROM messages WHERE id = %s", [id])
+
+# # delete all messages
+# def delete_all_message(messages_user_id_fk):
+#     database.sql_write("DELETE FROM messages WHERE id = %s", [id])
